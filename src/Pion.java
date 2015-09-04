a74e37fb4c78375857b4c91177343d2b2abef34b
Sawah <rayanlesage@gmail.com>
2015-09-04 14:14:24 +0200

    Update Pion.java

--- src/Pion.java
+++ src/Pion.java
@@ -65,4 +65,94 @@ public class Pion
 	public boolean estCouleurOpposee(Pion p){
 		return p.getCouleur() == this.getCouleur()*-1;
 	}
+	
+	public String estPosable(Plateau plateau, int joueur) {
+		String rslt = "";
+		Pion pionEnvironnant;
+		Pion pionEloigne;
+		Pion pionPrecedent;
+		boolean encoreUnApres = false;
+		int cpt;
+		
+		// Verifier la couleur du joueur
+		int couleur;
+		if (joueur == 1) {
+			couleur = 1;
+		} else {
+			couleur = -1;
+		}
+
+		
+		// Le pion capture-t-il des pions ennemis ? Differenciez observations cases alentours selon position (centrale, coin, bordure)
+		if (this.estDansPlateau(plateau)) {
+		// Coin
+		
+		
+		// Bordure
+		
+		
+		// Centrale
+		
+			for (int i = -1; i <= 1; i++) {
+				
+				for (int j = -1; j <= 1; j++) {
+					cpt = 2;
+					pionEnvironnant = plateau.getPion(this.getX()+i, this.getY()+j);
+					System.out.println(pionEnvironnant.getX() + " " + pionEnvironnant.getY());
+					
+					if (i == 0 && j == 0) {
+						// Ne rien faire... Barbare, peut-etre
+						
+					} else if (couleur == pionEnvironnant.getCouleur() * -1) { 
+						rslt+=this.getX() + " " + this.getY() + " ";
+						rslt+=pionEnvironnant.getX() + " " + pionEnvironnant.getY() + " ";
+						encoreUnApres = true;
+						
+						while(encoreUnApres) {
+							System.out.println(this.getX() + " " + this.getY() + " " + i + " " + j + " " + cpt);
+							pionEloigne = plateau.getPion((this.getX() + i*cpt), (this.getY() + j*cpt));
+							pionPrecedent = plateau.getPion(this.getX() + i*(cpt-1), this.getY() + j*(cpt-1));
+							if (pionEloigne.estDansPlateau(plateau)) {
+								
+								if (couleur == pionEloigne.getCouleur()*-1) {
+									rslt+= (this.getX() + i*cpt) + " " + (this.getY() + j*cpt) + " ";
+									cpt++;
+								} else if (couleur == pionEloigne.getCouleur()*-1 
+										&& pionEloigne.getX() == 0 || pionEloigne.getX() == plateau.tab.length 
+											|| pionEloigne.getY() == 0 || pionEloigne.getY() == plateau.tab[0].length
+										&& pionPrecedent.getX() != 0 || pionPrecedent.getX() != plateau.tab.length 
+											|| pionPrecedent.getY() != 0 || pionPrecedent.getY() != plateau.tab[0].length
+										) {
+									rslt = "";
+									encoreUnApres = false;
+									
+								} else if (pionEloigne.getCouleur() == 0) {
+									rslt="";
+									encoreUnApres = false;
+									
+								} else if (couleur == pionEloigne.getCouleur()) {
+									rslt+= (this.getX() + i*cpt) + " " + (this.getY() + j*cpt);
+									encoreUnApres = false;
+									plateau.inverserLigne(rslt);
+								}
+							} else {
+								encoreUnApres = false;
+								rslt = "";
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return rslt;
+	}
+	
+	public boolean estDansPlateau(Plateau plateau) {
+		return this.getX() >= 0 || this.getX() < plateau.tab.length || this.getY() >= 0 || this.getY() < plateau.tab[0].length;
+	}
+	
+	public boolean estVide() {
+		return this.getCouleur() == 0;
+	}
 }
